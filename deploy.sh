eaa626467c698a52b3affdb0e8d72d378b3cfc7e
Olusegun Adesanya <adesanyaolusegun@gmail.com>
2019-03-22 11:16:27 +0100

    chore(StagingDeployment): add staging process

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
-
 if [ "$CIRCLE_BRANCH" == 'master' ] || [ "$CIRCLE_BRANCH" == 'v2-master' ] || ! [ -z "$CIRCLE_TAG" ]; then
+  DEPLOYMENT_ENVIRONMENT=$DEPLOYMENT_ENVIRONMENT_PROD
+  GCLOUD_SERVICE_KEY=$GCLOUD_SERVICE_KEY_PROD
+  PROJECT_NAME=$PROJECT_NAME_PROD
+  CLUSTER_NAME=$CLUSTER_NAME_PROD
+  CLOUDSDK_COMPUTE_ZONE=$CLOUDSDK_COMPUTE_ZONE_PROD
+  DEPLOYMENT_CHANNEL="#deploy-prod"
+
+elif ["$CIRCLE_BRANCH" == 'develop']; then
   DEPLOYMENT_ENVIRONMENT=$DEPLOYMENT_ENVIRONMENT
   GCLOUD_SERVICE_KEY=$GCLOUD_SERVICE_KEY
   PROJECT_NAME=$PROJECT_NAME
@@ -7,6 +14,24 @@ if [ "$CIRCLE_BRANCH" == 'master' ] || [ "$CIRCLE_BRANCH" == 'v2-master' ] || ! 
   CLOUDSDK_COMPUTE_ZONE=$CLOUDSDK_COMPUTE_ZONE
   DEPLOYMENT_CHANNEL="#deploy-staging"
 fi
+
+# set key and authenticate gcloud
+echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
+sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+# configure gcloud
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set project $PROJECT_NAME
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+
+sudo apt-get install -y python-pip
+
+mkdir lib
+pip install -t lib -r requirements.txt
+gcloud app --quiet deploy --version=1 cron.yaml app.yaml
+
+
 
 # set key and authenticate gcloud
 echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
