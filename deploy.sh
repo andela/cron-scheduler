1e4927e3751fe59cf94284b88a2ac8b7337f75f4
Olusegun Adesanya <adesanyaolusegun@gmail.com>
2019-03-22 12:07:22 +0100

    chore(StagingDeployment): remove duplicates

--- deploy.sh
+++ deploy.sh
@@ -30,22 +30,3 @@ sudo apt-get install -y python-pip
 mkdir lib
 pip install -t lib -r requirements.txt
 gcloud app --quiet deploy --version=1 cron.yaml app.yaml
-
-
-
-# set key and authenticate gcloud
-echo $GCLOUD_SERVICE_KEY | base64 --decode > ${HOME}/gcloud-service-key.json
-sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-
-# configure gcloud
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set project $PROJECT_NAME
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster $CLUSTER_NAME
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials $CLUSTER_NAME
-
-sudo apt-get install -y python-pip
-
-mkdir lib
-pip install -t lib -r requirements.txt
-gcloud app --quiet deploy --version=1 cron.yaml app.yaml
-
