52578134c6ccccea5928ac4fe4be537fd058da17
George Obiero <george.obiero@andela.com>
2019-04-16 17:33:48 +0300

    Clean up ignored files

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ import sys
 sys.path.append("./lib")
 
 import time
+import os
+import yaml
 
 import tornado.gen
 import tornado.ioloop
@@ -25,22 +27,47 @@ from nats.io.client import Client as NATS
 
 class NatsHandler(webapp2.RequestHandler):
     def get(self, event_type):
-        tornado.ioloop.IOLoop.current().run_sync(self.handler)
+        tornado.ioloop.IOLoop.current().run_sync(lambda: self.handler(event_type))
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps({"status": "200"}))
 
     @tornado.gen.coroutine
-    def handler(arg):
+    def handler(arg, event_type):
         nc = NATS()
         try:
-            # inject url dynamically
-            opts = {"servers": ["nats://sandbox-nats.my.andela.com:4222"]}
+            natServers = {
+                "sandbox": "nats://sandbox-nats.my.andela.com:4222",
+                "staging": "nats://staging-nats.my.andela.com:4222",
+                "qa": "nats://qa-nats.my.andela.com:4222",
+                "production": "nats://nats.my.andela.com:4222"
+            }
+
+            source = yaml_to_json(os.getcwd() + "/template.cron.yaml")
+            servers = []
+            for task in source["cron"]:
+                if event_type in task['url']:
+                    environments = task['targetEnvironment'].split(",")
+                    for environment in environments:
+                        servers.append(natServers[environment.strip()])
+
+            opts = {"servers": servers}
             yield nc.connect(**opts)
-            yield nc.publish("Test", "{}")
+            message = {
+                "eventType": event_type
+            }
+            yield nc.publish("crons", str(message))
             yield nc.flush()
-            print("Successfully published to '{0}', message: {1}".format("Test", {}))
+            print("Successfully published to '{0}', message: {1}".format("crons", message))
         except Exception, e:
             print(e)
+
+def yaml_to_json(sourceFile):
+    with open(sourceFile, "r") as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return False
 
 app = webapp2.WSGIApplication([
     webapp2.Route(r'/publish/<event_type>', handler=NatsHandler)
